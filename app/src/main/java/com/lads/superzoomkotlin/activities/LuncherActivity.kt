fa3f2259a5f4b9a1ef28d0b0096eca9e8bfb6973
basitzawar <basitzawar@gmail.com>
2023-03-14 13:43:07 +0500

    14323

--- app/src/main/java/com/lads/superzoomkotlin/activities/LuncherActivity.kt
+++ app/src/main/java/com/lads/superzoomkotlin/activities/LuncherActivity.kt
@@ -2,19 +2,31 @@ package com.lads.superzoomkotlin.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+import android.os.Looper
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.isVisible
 import com.lads.superzoomkotlin.R
 
 class LuncherActivity : AppCompatActivity() {
-    private var button: Button? = null
+    private lateinit var btnGetStarted: AppCompatButton
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_luncher)
 
-        button = findViewById(R.id.btnGetStarted)
-        button?.setOnClickListener {
+        btnGetStarted = findViewById(R.id.btnGetStarted)
+        progressBar = findViewById(R.id.progressBar)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            progressBar.isVisible = false
+            btnGetStarted.isVisible = true
+        }, 3000)
+
+        btnGetStarted?.setOnClickListener {
             val intent = Intent(
                 this, MainActivity::class.java
             )
